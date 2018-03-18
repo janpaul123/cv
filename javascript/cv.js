c9efa21e67dd56b4448751d8a80349a53dad4658
Jan Paul Posma <janpaul123@users.noreply.github.com>
2018-03-17 17:22:40 -0700

    Minor update

--- javascript/cv.js
+++ javascript/cv.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-	$('.about > li').click(function(event) {
-		if (!$(event.target).is('a')) {
-			$(this).children('.category').toggle('fast');
-			event.stopImmediatePropagation();
-		}
-	});
-
 	$('.category > li').click(function(event) {
 		if (!$(event.target).is('a')) {
 			$children = $(this).children('.item');
